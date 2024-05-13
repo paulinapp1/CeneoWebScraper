9e141d4c50b99b1475a15398deb1a6f652af5406
Paulina Grabska <paulinagrabskainf@gmail.com>
2024-05-13 14:46:43 +0200

    2024-05-13 PG

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
 
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST','GET'])
 def extract():
+    if request.method=="POST":
+        product_id=request.form.get('product_id')
+        return redirect(url_for("product", product_id=product_id))
     return render_template("extract.html")
 @app.route('/products')
 def products():
