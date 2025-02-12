53f69aced8039b68bac34aacbdf21ab72ebaf67d
Paulina Grabska <paulinagrabskainf@gmail.com>
2025-02-12 12:54:51 +0100

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
-from flask import Flask,g,request, session
-from flask_babel import Babel
+from flask import Flask
+import os
 
 app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 5000))
 
 
 
 
 from app import routes
 
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port, debug=False)  
